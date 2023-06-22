f27474222c02af84f65f370890b4d2e8491f9fa0
Matheus Gonçalves <69082886+matheusgoncalvess@users.noreply.github.com>
2023-06-22 12:01:07 -0300

    update commit

---
+++ 03-sum.py
@@ -0,0 +1,6 @@
+primeiroNumero = int(input('Digite o primeiro numero: '))
+segundoNumero = int(input('Digite o segundo número: '))
+
+soma = (primeiroNumero + segundoNumero)
+
+print('A soma é', soma)
