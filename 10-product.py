2901d191e47f5baf2f6361e3a315a066dce59289
Matheus Gonçalves <69082886+matheusgoncalvess@users.noreply.github.com>
2023-06-27 17:20:24 -0300

    add files

---
+++ 10-product.py
@@ -0,0 +1,7 @@
+first_number = int(input('Digite o primeiro numero: '))
+second_number = int(input('Digite o segundo numero: '))
+number = float(input('Digite um número real: '))
+
+print('O produto do dobro do primeiro com a metade do segundo é', round((first_number * 2) / second_number))
+print('A soma do triplo do primeiro com o terceiro é', round(first_number ** 3  + number))
+print('O terceiro elevado ao cubo é', number ** number)
---
+++ 11-peso_ideal.py
@@ -0,0 +1,6 @@
+altura = float(input('Digite a sua altura: '))
+peso = 72.7
+
+peso_ideal = peso * altura - 58
+
+print('O seu peso ideal é', round(peso_ideal))
